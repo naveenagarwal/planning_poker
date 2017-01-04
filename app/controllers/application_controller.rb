5f60d89cc1684fa481dc644163d5419b36391d75
JesonDias <jeson.114@gmail.com>
2017-01-04 19:12:07 +0530

    basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::API
   before_filter :set_headers
 
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user.present?
+      render json: {  success: true, email: user.email, name: user.name }
+    else
+      render json: {  success: false, status: 422 }
+    end
+
+  end
+
   private
 
   def set_headers
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :sprints
   resources :projects
   resources :organizations
+
+  post '/login', to: "application#login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
